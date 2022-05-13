de335b7198bde8a0a6bd302bf7f660b447011622
sonam chand <89241660+Sonamchandji@users.noreply.github.com>
2022-05-14 08:35:07 +1000

    Update and rename data simulate.py to gcs_data_simu_pbsb.py

---
+++ gcs_data_simu_pbsb.py
@@ -0,0 +1,109 @@
+import time
+import csv
+import json
+import os
+import requests as rq
+import logging
+import argparse
+import datetime
+from google.cloud import pubsub
+from google.cloud import pubsub_v1
+from csv import reader
+from google.cloud import storage
+dir = os.getcwd()
+bucket_name='gs://finfo-2022/pe_data/raw_pe_data.csv'
+os.system('gsutil cp '+ bucket_name  +' '+ dir)
+data_file = os.path.join(dir,'raw_pe_data.csv')
+
+TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+TOPIC = 'my_pub_topic1'
+INPUT = 'raw_pe_data.csv'
+
+def publish(publisher, topic, events):
+   numobs = len(events)
+   if numobs > 0:
+       logging.info('Publishing {0} events from {1}'.format(numobs, get_timestamp(events[0])))
+       for event_data in events:
+         publisher.publish(topic,event_data)
+def get_timestamp(line):
+    ## convert from bytes to str
+    line = line.decode('utf-8')
+    # look at first field of row
+    timestamp = line.split(',')[0]
+    return datetime.datetime.strptime(timestamp, TIME_FORMAT)
+
+
+def simulate(topic, ifp, firstObsTime, programStart, speedFactor):
+       # sleep computation
+       def compute_sleep_secs(obs_time):
+          time_elapsed = (datetime.datetime.utcnow() - programStart).seconds
+          sim_time_elapsed = ((obs_time - firstObsTime).days * 86400.0 + (obs_time - firstObsTime).seconds) / speedFactor
+          to_sleep_secs = sim_time_elapsed - time_elapsed
+          return to_sleep_secs
+
+       topublish = list()
+       for line in ifp:
+         event_data = line   # entire line of input CSV is the message
+         obs_time = get_timestamp(line) # from first column
+
+       # how much time should we sleep?
+       if compute_sleep_secs(obs_time) > 1:
+          # notify the accumulated topublish
+          publish(publisher, topic, topublish) # notify accumulated messages
+          topublish = list() # empty out list
+
+          # recompute sleep, since notification takes a while
+          to_sleep_secs = compute_sleep_secs(obs_time)
+       if to_sleep_secs > 0:
+             logging.info('Sleeping {} seconds'.format(to_sleep_secs))
+             time.sleep(to_sleep_secs)
+             topublish.append(event_data)
+
+   # left-over records; notify again
+             publish(publisher, topic, topublish)
+def peek_timestamp(ifp):
+      # peek ahead to next line, get timestamp and go back
+      pos = ifp.tell()
+      line = ifp.readline()
+      ifp.seek(pos)
+      return get_timestamp(line)
+speedFactor=60
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Send sensor data to Cloud Pub/Sub in small groups, simulating real-time behavior')
+    parser.add_argument('--speedFactor', help='Example: 60 implies 1 hour of data sent to Cloud Pub/Sub in 1 minute', required=True, type=float)
+    parser.add_argument('--project', help='Example: --project $DEVSHELL_PROJECT_ID', required=True)
+    args = parser.parse_args()
+
+    # create Pub/Sub notification topic
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+publisher = pubsub.PublisherClient()
+event_type = publisher.topic_path(args.project,TOPIC)
+try:
+    publisher.get_topic(event_type)
+    logging.info('Reusing pub/sub topic {}'.format(TOPIC))
+except:
+    publisher.create_topic(event_type)
+    logging.info('Creating pub/sub topic {}'.format(TOPIC))
+
+    # notify about each line in the input file
+programStartTime = datetime.datetime.utcnow()
+f_data = open(data_file)
+flag=0
+fieldnames=("date","company_name","growth_stage","country","industry",
+            "sub_industry","client__focus","business_model",
+            "company_status","round","amount_raised","currency",
+            "date","quarter","Month","Year","investors","investor_types","company_valuation_usd",
+            "valuation_date")
+reader = csv.DictReader( f_data, fieldnames)
+
+for row in reader:
+    if flag==0:
+       flag=1
+       pass
+    else:
+#with gzip.open(INPUT, 'rb') as ifp:
+#header = ifp.readline()  # skip header
+       firstObsTime = peek_timestamp(row)
+       logging.info('Sending sensor data from {}'.format(firstObsTime))
+       simulate(event_type, row, firstObsTime, programStartTime, args.speedFactor)
+
