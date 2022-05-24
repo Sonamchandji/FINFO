8d2c3d4578ec98d81c0f3d67dcc527607c197622
sonam chand <89241660+Sonamchandji@users.noreply.github.com>
2022-05-24 23:14:34 +1000

    Update pe_pb_df_bq.py

--- pe_pb_df_bq.py
+++ pe_pb_df_bq.py
@@ -69,4 +69,5 @@ def run():
                                                                                                                                                                                                                                                    )
            )
 if __name__ == "__main__":
+    run()
                                                                                                                                                                                     run()
